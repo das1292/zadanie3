942c37fe5f66e23068e3156830a8048eba18caac
das1292 <jakub.klos.10@wp.pl>
2021-03-17 14:00:27 +0100

    zadanie3

--- main.py
+++ main.py
@@ -23,19 +23,44 @@
 # def pit(x, y, z):
 #  przy = x**2 + y**2
 #  przeciw = z**2
-#  if przy == przeciw:
-#    print("Trójkąt o bokach", x, y, z, "jest trójkątem prostokątnym.")
+#   if przy == przeciw:
+#       print("Trójkąt o bokach", x, y, z, "jest trójkątem prostokątnym.")
 #     return 1
-#  else:
-#    print("Trójkąt o bokach", x, y, z, "nie jest trójątem prostokątnym.")
-#     return 0
+#   else:
+#       print("Trójkąt o bokach", x, y, z, "nie jest trójątem prostokątnym.")
+#           return 0
 # print(pit(3,4,5))
 # print(pit(2,4,5))
 
 #Zadanie5
 # def trapez(a=2, b=4, h=3):
-#  P = ((a+b)*h)/2
-#  print("Pole: ")
-#     return P
+#   P = ((a+b)*h)/2
+# print("Pole: ")
+#   return P
 # print(trapez())
+
+#Zadanie6
+# def ciag1(a1=1, y=4, ile=10):
+#   for i in range(ile):
+#           a1 *= y
+#       return a1
+# print(ciag1())
+
+#Zadanie7
+# def ciag2(a1=1, y=4, ile=10):
+#     a1 = (a1*y)**ile
+#     return a1
+# print(ciag2())
+
+#Zadanie8
+# zakupy = {"ziemniaki": 4, "chispy": 7, "pizza": 15}
+# def list(a):
+#     b = 0
+#     c = 0
+#     for x in a.values():
+#         b = b + x
+#         c += 1
+#     print('Wartość zakupów to',b, "zł.\nWszyskich produktów jest: ")
+#     return c
+# print(list(zakupy))
 
